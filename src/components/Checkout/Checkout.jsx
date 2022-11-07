51020b21ef311c1f33c35edf07fe98dc83974c71
Marcelo Oviedo <oviedommarcelo@gmail.com>
2022-11-07 10:44:08 -0300

    Fix order in Firestore

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,103 +1,101 @@
-import { useContext, useState } from "react"
-import { CartContext } from "../../context/CartContext"
 import { addDoc, collection, getDocs, query, where, documentId, writeBatch } from "firebase/firestore"
 import { db } from "../../services/firebase"
-import { Loader } from "../Loader/Loader"
+import { Loader } from "../Loader/Loader" 
+import { useState, useContext } from "react"
+import { CartContext } from "../../context/CartContext"
 import { useNavigate } from "react-router-dom"
-import OrderForm from "../Form/Form"
+import ClientForm from '../Form/Form'
+import Swal from "sweetalert2";
+import {useOrders} from "../../services/firestore/Orders"
 
 
-const CheckOut = () => {
+const CheckOut=()=>{
 
-    const { cart, total, clearCart } = useContext(CartContext)
-    const [loading, setLoading] = useState(false)
-    const navigate = useNavigate()
+    const {clearCart} = useContext(CartContext)
+
     const [personalData, setPersonalData] = useState(false)
-    const [datos, setDatos] = useState({})
-    const fullData = (name, phone, email, adress) => {
-        setDatos({ name, phone, email, adress })
+
+    const [orderData, setOrderData] = useState()
+
+    const DataCompleted = (declaredName, declaredAddress, declaredPhone, declaredEmail) =>{
+        
+        setOrderData({declaredName, declaredAddress, declaredPhone, declaredEmail})
+        
         setPersonalData(true)
     }
+    
+    const [loading, setLoading] = useState(false)
+    
+    const navigate = useNavigate ()
+    
+    const { createOrder } = useOrders()
 
-
-    /* CREATE ORDER WITH MOCK */
-    const createOrder = async (salesData) => {
+    const getOrder =()=>{    
+        
         setLoading(true)
 
-        try {
-            const objOrder = {
-                buyer: {
-                    buyer: salesData,
-                },
-                items: cart,
-                total: total
-            }
-            const collectionRef = collection(db, 'orders')
-            addDoc(collectionRef, objOrder).then(response => {
-                console.log(response.id)
-            }).catch(error => {
-                console.log(error)
-            })
-
-            const batch = writeBatch(db)
-            const outOfStock = []
-            const ids = cart.map(prod => prod.id) /* Obtain all products Ids */
-            const productRef = collection(db, 'products') /* Obtain all products */
-            const productsAddedFromFirestore = await getDocs(query(productRef, where(documentId(), 'in', ids))) /* Filter all products by ID and wait for the answer*/
-
-            const { docs } = productsAddedFromFirestore /* Obtain docs */
-
-            docs.forEach(doc => {
-                const dataDoc = doc.data() /* Data of docs */
-                const stockDb = dataDoc.stock
-                const productAddedToCart = cart.find(prod => prod.id === doc.id)
-                const prodQuantity = productAddedToCart?.quantity
-
-                if (stockDb >= prodQuantity) {
-                    batch.update(doc.ref, { stock: stockDb - prodQuantity }) /* Direct reference to the document (doc.ref)*/
-
-                } else {
-                    outOfStock.push({ id: doc.id, ...dataDoc })
-                }
-
-            })
-
-            if (outOfStock.length === 0) { /* Operation succesfull*/
-                await batch.commit()
-                const orderRef = collection(db, 'orders')
-                const orderAdded = await addDoc(orderRef, objOrder)
+        createOrder(orderData).then(response => {
+            if(response.result === 'orderCreated') {
                 clearCart()
-                setTimeout(() => {
-                    navigate('/')
-                }, 3000)
-                console.log(`El Id de su orden es: ${orderAdded.id}`)
-            } else {
-                console.log('Hay productos que estan fueras de stock')
+
+                Swal.fire({
+                    title:"COMPRA REALIZADA CON ÉXITO",
+                    text:`Enviamos el código de compra "${response.id}" 
+                    a 
+                    ${orderData.declaredEmail}`,
+                    icon: false,
+                    buttons: true,
+                    dangerMode: true,
+                    timer:6000,
+                    customClass:"swAlert"
+                })
+        
+                navigate ('/')
+            }else{
+                Swal.fire({
+                    title:"ERROR",
+                    text:`Uno de los productos seleccionados ya no está en stock`,
+                    icon: false,
+                    buttons: true,
+                    dangerMode: true,
+                    timer:6000,
+                    customClass:"swAlert"
+                })
+                navigate ('/cart')
             }
-
-
-        } catch (error) {
-            console.log(error)
-        } finally {
+   
+        }).catch(error => {
+            Swal.fire({
+                title:`${error}`,
+                icon: false,
+                buttons: true,
+                dangerMode: true,
+                timer:6000,
+                customClass:"swAlert"
+            })
+        }).finally(() => {
             setLoading(false)
-        }
-
+        })
     }
 
-    if (loading) {
-        return <Loader message={'Se esta generando orden, por favor aguarde'} />
-    }
-
+if (loading){
 
     return (
         <div>
-            <h1>Checkout</h1>
-            <OrderForm fullData={fullData} />
-            {personalData
-            ?< button onClick={createOrder}>Generar orden</button>
-            :""
-        }
-        </div >
+        <Loader message={'Validando stock'}/>
+        </div>
     )
 }
+
+return (
+
+    <div>
+        <h1>COMPLETÁ TUS DATOS</h1>
+        <ClientForm DataCompleted={DataCompleted}/>
+            { personalData?<button className="comprarBtn" type="submit" onClick={getOrder}>CONFIRMAR COMPRA</button>
+        : ""}
+    </div>
+)
+}
+
 export default CheckOut;
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,51 +1,80 @@
-import Swal from "sweetalert2";
-import '../Form/Form.css'
-import { useState, createContext } from "react";
+import { Form, Button } from 'react-bootstrap';
+import { useState} from "react"
+import './Form.css'
+import { ToastContainer, toast, Zoom } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-export const CheckoutForm = createContext({
-  name:"",
-  email:"",
-  phone:"",
-  address:""
-}) 
-
-
-const OrderForm = ({fullData}) => {
-    
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phone, setPhone] = useState("");
-  const [address, setAddress] = useState("");
+const ClientForm = ({DataCompleted}) => {
+    const [declaredName, setName] = useState("");
+    const [declaredAddress, setAddress] = useState("");
+    const [declaredPhone, setPhone] = useState("");
+    const [declaredEmail, setEmail] = useState("");
+    const [declaredEmail2, setEmail2] = useState("");
 
 
+    const submitHandler = (event) => {
+        event.preventDefault();
+        
+        if (!declaredName){
+            alertFn("Por favor, completa correctamente este campo", "alertName")
+        }
+        else if (!declaredAddress){
+            alertFn("Por favor, completa correctamente este campo", "alertAddress")
+        } 
+        else if (!declaredPhone){
+            alertFn("Por favor, completa correctamente este campo", "alertPhone")
+        }
+        else if (declaredEmail !== declaredEmail2 || !declaredEmail ||!declaredEmail2){
+            alertFn("Los correos electrónicos deben coincidir", "alertMail")
+        } else{
+            DataCompleted(
+                declaredName, declaredAddress, declaredPhone, declaredEmail
+            )
+        }
+    };
+return(
 
-
-const submit = (e) => {
-  e.preventDefault ();
-
-  fullData(
-      name,
-      phone,
-      email,
-      address
-  )}
-
-
-  return (
-    <div>
-      <form className="main-form">
-        <input value={name} onChange={(e) => setName(e.target.value)} type="text" pattern="[a-zA-Z ]{1,35}"   className="form-input"   placeholder="Nombre" required />
-        <input value={email} onChange={(e) => setEmail(e.target.value)} type="email"  className="form-input"   placeholder="Email" required/>
-        <input value={phone}onChange={(e) => setPhone(e.target.value)} type="number" className="form-input"   placeholder="Teléfono"required />
-        <input value={address}onChange={(e) => setAddress(e.target.value)}type="text"   className="form-input"   placeholder="Dirección"required />
-      </form>
-
-      <div>
-        <button className="submit-button" onClick={submit}>Registrar Compra</button>
-      </div>
-    </div>
-  )
+    <Form onSubmit={submitHandler}>
+        <Form.Group className="divLabel" controlId="form.Name">
+            <Form.Label></Form.Label>
+            <Form.Control type="text" className="inputs" value={declaredName} pattern="[a-zA-Z ]{1,35}" onChange={(e) => setName(e.target.value)} placeholder="Nombre y Apellido"/>
+        </Form.Group>
+        <Form.Group className="divLabel" controlId=".form.Name">
+            <Form.Label></Form.Label>
+            <Form.Control type="text" className="inputs" pattern="[a-zA-Z 0-9]{1,35}" value={declaredAddress} onChange={(e) => setAddress(e.target.value)} placeholder="Dirección"/>
+        </Form.Group>
+        <Form.Group className="divLabel" controlId="form.Name">
+            <Form.Label></Form.Label>
+        <Form.Control type="number" pattern="[0-9]" className="inputs" value={declaredPhone} onChange={(e) => setPhone(e.target.value)} placeholder="Teléfono" />
+        </Form.Group>
+        <Form.Group className="divLabel" controlId="form.Name">
+            <Form.Label></Form.Label>
+            <Form.Control type="email" className="inputs" value={declaredEmail} onChange={(e) => setEmail(e.target.value)} placeholder="Correo Electrónico" />
+        </Form.Group>
+        <Form.Group className="divLabel" controlId="form.Name">
+            <Form.Label></Form.Label>
+            <Form.Control type="email" className="inputs" value={declaredEmail2} onChange={(e) => setEmail2(e.target.value)} placeholder="Reingresa Correo Electrónico" />
+            <ToastContainer limit={1}/>
+        </Form.Group>
+         <div className="divLabel">
+        <Button className="submitBtn"type='submit'>Confirmar Datos</Button>
+        </div> 
+    </Form>
+)
 }
 
+const alertFn = (a, b) => toast(a, {
+    position: "top-right",
+    autoClose: 2000,
+    hideProgressBar: true,
+    closeOnClick: false,
+    pauseOnHover: true,
+    transition: Zoom,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+    className:b
+    });
 
-export default OrderForm;
+
+export default ClientForm
